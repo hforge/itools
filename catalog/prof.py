0e635500d4a97820ff63d627df949bdae85aad49
J. David Ibanez <jdavid@itaapy.com>
2004-09-11 18:19:50 +0000

    use Python's documentation to profile the catalog
    Now the catalog profiling code uses the Python's documentation
    as its corpus, which is bigger and better to test the catalog's
    performance.
    
    Today the path to the Python's docs is hardcoded, so you may need
    to change it to make the code work
    git-archimport-id: jdavid@itaapy.com--public/itools--main--0.4--patch-80

--- catalog/prof.py
+++ catalog/prof.py
@@ -21,21 +21,24 @@ import profile
 
 # Import from itools
 from itools.handlers import get_handler, Text
+from itools.resources import get_resource
+from itools.xml import HTML
 from itools.catalog.Catalog import Catalog
 
 
 
+docs_path = '/usr/share/doc/python-docs-2.3.4/html/lib'
 
-class Document(Text.Text):
-    def _load(self, resource):
-        # Pre-process (load as unicode)
-        Text.Text._load(self, resource)
-        data = self._data
-        del self._data
-        # Extract the title and body
-        lines = data.split('\n')
-        self.title = lines[0]
-        self.body = '\n'.join(lines[3:])
+
+class Document(HTML.Document):
+    def title(self):
+        head = self.get_head()
+        return unicode(head.get_elements('title')[0].children)
+
+
+    def body(self):
+        return unicode(self.get_body().children)
+
 
 
 # Create and get a new empty index
@@ -48,13 +51,27 @@ tests.set_handler('catalog', catalog)
 catalog_resource = tests.get_resource('catalog')
 catalog = Catalog(catalog_resource)
 
-resource_names = [ x for x in tests.get_resources() if x.endswith('.txt') ]
+
+src = get_resource(docs_path)
+resource_names = [ x for x in src.get_resources() if x.endswith('.html') ]
 resource_names.sort()
-documents = [ Document(tests.get_resource(x)) for x in resource_names ]
+documents = []
+for name in resource_names[:120]:
+    try:
+        doc = Document(src.get_resource(name))
+    except:
+        pass
+    else:
+        documents.append(doc)
+print len(documents)
+
+
+catalog = Catalog(fields=[('title', 'text', True, True),
+                          ('body', 'text', True, False)])
 
 
 def profile_indexing():
-    for document in documents:
+    for document in documents[:25]:
         catalog.index_document(document)
 
 

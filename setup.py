267b0a7d8a627eb2dd2b06c68cea493092e15ef5
J. David Ibanez <jdavid@itaapy.com>
2005-01-18 17:58:48 +0000

    get ready to release 0.6.1
    git-archimport-id: jdavid@itaapy.com--public/itools--main--0.6--patch-42

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ tools are:
 """
 
 setup(name = "itools",
-      version = "0.6.0",
+      version = "0.6.1",
       author = "J. David Ibáñez",
       author_email = "jdavid@itaapy.com",
       license = "GNU Lesser General Public License",

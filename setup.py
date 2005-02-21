cca155cf297b9dd84712f89134fac5ed519ef95e
J. David Ibanez <jdavid@itaapy.com>
2005-02-21 08:37:43 +0000

    merge from Luis branch (use distutils better)
    Patches applied:
    
     * luis@itaapy.com--private/itools--luis--0.7--base-0
       tag of jdavid@itaapy.com--public/itools--main--0.7--patch-16
    
     * luis@itaapy.com--private/itools--luis--0.7--patch-1
       Add a commented line to allow a documentation with less pages
    
     * luis@itaapy.com--private/itools--luis--0.7--patch-2
       add a Changelog file in the source tree
    
     * luis@itaapy.com--private/itools--luis--0.7--patch-3
       Add the Changelog file in the './site-paquages/itools' Folder
    
     * luis@itaapy.com--private/itools--luis--0.7--patch-4
       Add documentation to setup.py and do clean reindent
    
     * luis@itaapy.com--private/itools--luis--0.7--patch-5
       Improve the MANIFEST.in (no more warnings in 'python setup.py sdist') 
    
    git-archimport-id: jdavid@itaapy.com--public/itools--main--0.7--patch-20

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # -*- coding: ISO-8859-1 -*-
 # Copyright (C) 2002-2004 Juan David Ibáñez Palomar <jdavid@itaapy.com>
+#                    2005 Luis Belmar Leteliet <luis@itaapy.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -15,68 +16,36 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 
+"""
+This script can be tested with the folowing::
+
+  sudo rm -r /usr/lib/python2.3/site-packages/itools
+  sudo make clean 
+  python setup.py -q clean sdist
+  cd dist
+  tar xzf itools-0.6.4.tar.gz
+  cd itools-0.6.4
+  sudo python setup.py -q install
+
+We test the fact that Changelog, txt and dtml file are in site-packages::
+
+  ls -lah /usr/lib/python2.3/site-packages/itools/Changelog
+  find /usr/lib/python2.3/site-packages/itools/ -name "*.dtml"
+  find /usr/lib/python2.3/site-packages/itools/ -name "*.txt"
+"""
 
 
 # Import Python modules
 from distutils.command import build_py
 from distutils.core import setup
 from glob import glob
-import os
+import os, sys
 import string
 
 
-
-class build_py(build_py.build_py):
-    """
-    Provides a version of the Distutils 'build_py' command that knows about
-    DTML and text files.
-
-    Copied from quixote.qx_distutils.py
-
-    This bites -- way too much code had to be copied from
-    distutils/command/build.py just to add an extra file extension!
-    """
-
-    def find_package_modules(self, package, package_dir):
-        self.check_package(package, package_dir)
-        module_files = (glob(os.path.join(package_dir, "*.py")) +
-                        glob(os.path.join(package_dir, "*.dtml")) +
-                        glob(os.path.join(package_dir, "*.tex")) +
-                        glob(os.path.join(package_dir, "*.txt")))
-        modules = []
-        setup_script = os.path.abspath(self.distribution.script_name)
-
-        for f in module_files:
-            abs_f = os.path.abspath(f)
-            if abs_f != setup_script:
-                module = os.path.splitext(os.path.basename(f))[0]
-                modules.append((package, module, f))
-            else:
-                self.debug_print("excluding %s" % setup_script)
-        return modules
-
-
-    def build_module(self, module, module_file, package):
-        if isinstance(package, str):
-            package = package.split('.')
-        elif not isinstance(package, (list, tuple)):
-            raise TypeError, \
-                  "'package' must be a string (dot-separated), list, or tuple"
-
-        # Now put the module source file into the "build" area -- this is
-        # easy, we just copy it somewhere under self.build_lib (the build
-        # directory for Python source).
-        outfile = self.get_module_outfile(self.build_lib, package, module)
-        if module_file.endswith(".dtml"): # XXX hack for DTML
-            outfile = outfile[0:outfile.rfind('.')] + ".dtml"
-        elif module_file.endswith(".txt"): # XXX hack for TXT
-            outfile = outfile[0:outfile.rfind('.')] + ".txt"
-        elif module_file.endswith(".tex"): # XXX hack for TEX
-            outfile = outfile[0:outfile.rfind('.')] + ".tex"
-        dir = os.path.dirname(outfile)
-        self.mkpath(dir)
-        return self.copy_file(module_file, outfile, preserve_mode=0)
-
+distutils_path = sys.modules['distutils'].__path__[0]
+python_path, trash = os.path.split(distutils_path)
+itools_path = os.path.join(python_path, 'site-packages', 'itools')
 
 
 # XXX itools.zope.zmi shouldn't be a Python package, but this is the
@@ -119,20 +88,27 @@ setup(name = "itools",
       packages = ['itools', 'itools.catalog', 'itools.handlers', 'itools.i18n',
                   'itools.resources', 'itools.workflow', 'itools.xml',
                   'itools.zope'],
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-                   'Programming Language :: Python',
-                   'Topic :: Internet',
-                   'Topic :: Internet :: WWW/HTTP',
-                   'Topic :: Software Development',
-                   'Topic :: Software Development :: Internationalization',
-                   'Topic :: Software Development :: Libraries',
-                   'Topic :: Software Development :: Libraries :: Python Modules',
-                   'Topic :: Software Development :: Localization',
-                   'Topic :: Text Processing',
-                   'Topic :: Text Processing :: Markup',
-                   'Topic :: Text Processing :: Markup :: XML'],
-##      data_files = [('zope', ['zope/localroles.dtml'])],
+      classifiers = ['Development Status :: 3 - Alpha',
+                     'Intended Audience :: Developers',
+                     ('License :: OSI Approved :: GNU Library or Lesser General'
+                      ' Public License (LGPL)'),
+                     'Programming Language :: Python',
+                     'Topic :: Internet',
+                     'Topic :: Internet :: WWW/HTTP',
+                     'Topic :: Software Development',
+                     'Topic :: Software Development :: Internationalization',
+                     'Topic :: Software Development :: Libraries',
+                     ('Topic :: Software Development :: Libraries :: Python'
+                      ' Modules'),
+                     'Topic :: Software Development :: Localization',
+                     'Topic :: Text Processing',
+                     'Topic :: Text Processing :: Markup',
+                     'Topic :: Text Processing :: Markup :: XML'],
+        data_files = [(itools_path, ['Changelog']), 
+                      (os.path.join(itools_path, ('zope')), 
+                       ['zope/localroles.dtml']),
+                      (os.path.join(itools_path, ('i18n')), 
+                       ['i18n/languages.txt']), 
+                     ],
       scripts = ['i18n/igettext.py'],
-      cmdclass = {'build_py': build_py})
+      )

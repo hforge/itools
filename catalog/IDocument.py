9ea07b6762918c2952901a43788447ab98ba7490
J. David Ibanez <jdavid@itaapy.com>
2005-03-11 19:19:08 +0000

    catalog, add serialization methods to indexed documents
    git-archimport-id: jdavid@itaapy.com--public/itools--main--0.7--patch-46

--- catalog/IDocument.py
+++ catalog/IDocument.py
@@ -1,5 +1,5 @@
 # -*- coding: ISO-8859-1 -*-
-# Copyright (C) 2004 Juan David Ibáñez Palomar <jdavid@itaapy.com>
+# Copyright (C) 2004-2005 Juan David Ibáñez Palomar <jdavid@itaapy.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -16,7 +16,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 
 
-# Import from Python
+# Import from the Standard Library
 import datetime
 
 # Import from itools
@@ -27,6 +27,7 @@ import IO
 
 
 class IndexedField(File):
+
     def get_skeleton(self):
         return IO.encode_uint32(0)
 
@@ -51,8 +52,14 @@ class IndexedField(File):
         self.timestamp = self.resource.get_mtime()
 
 
+    def to_str(self):
+        return IO.encode_uint32(self.number_of_terms) \
+               + ''.join([ IO.encode_string(x) for x in self.terms ])
+
+
 
 class StoredField(File):
+
     def get_skeleton(self, data=u''):
         return IO.encode_string(data)
 
@@ -60,6 +67,10 @@ class StoredField(File):
     def _load(self, resource):
         data = resource.get_data()
         self.value = IO.decode_string(data)[0]
+
+
+    def to_str(self):
+        return IO.encode_string(self.value)
 
 
 

10e3494b023699fdcb2cbe973f15ae8a062173c1
J. David Ibanez <jdavid@itaapy.com>
2005-03-26 20:27:35 +0000

    uri, add new unit tests, improve tests coding
    git-archimport-id: jdavid@itaapy.com--public/itools--main--0.8--patch-40

--- test_uri.py
+++ test_uri.py
@@ -1,5 +1,5 @@
 # -*- coding: ISO-8859-1 -*-
-# Copyright (C) 2004 Juan David Ibáñez Palomar <jdavid@itaapy.com>
+# Copyright (C) 2004-2005 Juan David Ibáñez Palomar <jdavid@itaapy.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -25,6 +25,7 @@ import uri
 
 
 class PathTestCase(TestCase):
+
     def test_simplenorm(self):
         """
         Test the simple path normalization:
@@ -35,7 +36,7 @@ class PathTestCase(TestCase):
         - 'a/'    = 'a'
         """
         path = uri.Path('./a/./b//c/')
-        assert str(path) == 'a/b/c/'
+        self.assertEqual(str(path), 'a/b/c/')
 
 
     def test_backnorm(self):
@@ -43,7 +44,7 @@ class PathTestCase(TestCase):
         Test the normalization 'a/../b' = 'b'
         """
         path = uri.Path('a/b/c/../d')
-        assert str(path) == 'a/b/d'
+        self.assertEqual(str(path), 'a/b/d')
 
 
     def test_absnorm(self):
@@ -51,7 +52,7 @@ class PathTestCase(TestCase):
         Test the normalization '/..' = '/'
         """
         path = uri.Path('/../../a/b/c')
-        assert str(path) == '/a/b/c'
+        self.assertEqual(str(path), '/a/b/c')
 
 
     def test_relnorm(self):
@@ -59,7 +60,7 @@ class PathTestCase(TestCase):
         Check that '../' = '../'
         """
         path = uri.Path('../../a//.//b/c')
-        assert str(path) == '../../a/b/c'
+        self.assertEqual(str(path), '../../a/b/c')
 
 
 
@@ -67,40 +68,55 @@ class ParseTestCase(TestCase):
     """
     Tests to verify the correct parsing of generic references.
     """
+
     def test_full(self):
         ref = 'http://example.com/a/b/c?query#fragment'
         ref = uri.Reference(ref)
-        assert ref.scheme == 'http'
-        assert ref.authority == 'example.com'
-        assert ref.path == '/a/b/c'
-        assert ref.query == 'query'
-        assert ref.fragment == 'fragment'
+        self.assertEqual(ref.scheme, 'http')
+        self.assertEqual(ref.authority, 'example.com')
+        self.assertEqual(ref.path, '/a/b/c')
+        self.assertEqual(ref.query, 'query')
+        self.assertEqual(ref.fragment, 'fragment')
 
 
     def test_network(self):
         ref = '//example.com/a/b'
         ref = uri.Reference(ref)
-        assert bool(ref.scheme) is False
-        assert ref.authority == 'example.com'
-        assert ref.path == '/a/b'
+        self.assertEqual(bool(ref.scheme), False)
+        self.assertEqual(ref.authority, 'example.com')
+        self.assertEqual(ref.path, '/a/b')
 
 
     def test_path(self):
         ref = '/a/b/c'
         ref = uri.Reference(ref)
-        assert bool(ref.scheme) is False
-        assert bool(ref.authority) is False
-        assert ref.path == '/a/b/c'
+        self.assertEqual(bool(ref.scheme), False)
+        self.assertEqual(bool(ref.authority), False)
+        self.assertEqual(ref.path, '/a/b/c')
 
 
     def test_query(self):
         ref = '?query'
         ref = uri.Reference(ref)
-        assert bool(ref.scheme) is False
-        assert bool(ref.authority) is False
-        assert len(ref.path) == 0
-        assert ref.query == 'query'
+        self.assertEqual(bool(ref.scheme), False)
+        self.assertEqual(bool(ref.authority), False)
+        self.assertEqual(len(ref.path), 0)
+        self.assertEqual(ref.query, 'query')
 
+
+class SpecialTestCase(TestCase):
+    """Test special cases."""
+
+    def test_fragment(self):
+        self.assertEqual(str(uri.get_reference('#')), '#')
+
+
+    def test_dot(self):
+        self.assertEqual(str(uri.get_reference('.')), '.')
+
+
+    def test_empty(self):
+        self.assertEqual(str(uri.get_reference('')), '')
 
 
 
@@ -113,7 +129,8 @@ class ResolveTestCase(TestCase):
         self.base = uri.Reference('http://a/b/c/d;p?q')
 
 
-    def test(self):
+    def test_standard(self):
+        # Test the cases defined by RFC2396, section C.1
         failure = 0
         for reference, expected in [('g:h', 'g:h'),
                                     ('g', 'http://a/b/c/g'),
@@ -129,7 +146,7 @@ class ResolveTestCase(TestCase):
                                     (';x', 'http://a/b/c/;x'),
                                     ('g;x', 'http://a/b/c/g;x'),
                                     ('g;x?y#s', 'http://a/b/c/g;x?y#s'),
-                                    ('', 'http://a/b/c/'),
+                                    ('.', 'http://a/b/c/'),
                                     ('./', 'http://a/b/c/'),
                                     ('..', 'http://a/b/'),
                                     ('../', 'http://a/b/'),
@@ -139,13 +156,18 @@ class ResolveTestCase(TestCase):
                                     ('../../g', 'http://a/g')]:
             x = self.base.resolve(reference)
             try:
-                assert x == expected
+                self.assertEqual(x, expected)
             except AssertionError:
                 print '\n%s + %s = %s != %s' \
                       % (self.base, reference, x, expected)
                 failure += 1
         if failure:
             raise AssertionError, '%s uri resolutions failed' % failure
+
+
+    def test_others(self):
+        self.assertEqual(self.base.resolve(''), 'http://a/b/c/d;p?q')
+
 
 
 if __name__ == '__main__':

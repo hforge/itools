c7a92c319c5b658770bbe6680c46aecbc3c668af
J. David Ibanez <jdavid@itaapy.com>
2005-05-11 15:08:35 +0000

    merge html improvements from the 0.8 branch
    Patches applied:
    
     * jdavid@itaapy.com--public/itools--main--0.8--patch-91
       html, make a more robust parser
    
    git-archimport-id: jdavid@itaapy.com--public/itools--main--0.9--patch-38

--- html/parser.py
+++ html/parser.py
@@ -36,6 +36,27 @@ empty_elements = Set([
     # Vendor specific, not approved by W3C
     'embed'])
 
+# Elements whose end tag is optional
+optional_end_tag_elements = Set(['body', 'colgroup', 'dd', 'dt', 'head',
+                                 'html', 'li', 'option', 'p', 'tbody', 'td',
+                                 'tfoot', 'th', 'thead', 'tr'])
+
+# Elements whose end tag is optional and which can not contain a block tag
+# (hence must be closed before). XXX Finish.
+close_before_block = Set(['dd', 'dt', 'p'])
+
+
+# Block elements
+block_elements = Set([
+    'address', 'blockquote', 'center', 'dir', 'div', 'dl', 'fieldset', 'form',
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'isindex', 'menu', 'noframes',
+    'noscript', 'ol', 'p', 'pre', 'table', 'ul',
+    # Considered as block elements because they can contain block elements
+    'dd', 'dt', 'frameset', 'li', 'tbody', 'td', 'tfoot', 'th', 'thead', 'tr',
+    ])
+
+# Boolean attributes
+boolean_attributes = Set(['compact'])
 
 
 class Parser(HTMLParser):
@@ -44,6 +65,7 @@ class Parser(HTMLParser):
         self.encoding = 'UTF-8'
 
         self.events = []
+        self.stack = []
         self.feed(data)
         self.close()
         return self.events
@@ -57,20 +79,58 @@ class Parser(HTMLParser):
 
     def handle_starttag(self, name, attrs):
         line_number = self.getpos()[0]
+
+        # Close missing optional end tags
+        if self.stack and self.stack[-1] in close_before_block:
+            if name in block_elements:
+                element_name = self.stack.pop()
+                self.events.append((END_ELEMENT, element_name, line_number))
+
         # Start element
         self.events.append((START_ELEMENT, name, line_number))
 
         # Attributes
-        for attribute in attrs:
-            self.events.append((ATTRIBUTE, attribute, line_number))
+        for attribute_name, attribute_value in attrs:
+            if attribute_value is None:
+                if attribute_name in boolean_attributes:
+                    attribute_value = attribute_name
+                else:
+                    raise ValueError, \
+                          'missing attribute value for "%s"' % attribute_name
+            event = (ATTRIBUTE, (attribute_name, attribute_value), line_number)
+            self.events.append(event)
 
         # End element
         if name in empty_elements:
             self.events.append((END_ELEMENT, name, line_number))
+        else:
+            self.stack.append(name)
 
 
     def handle_endtag(self, name):
-        self.events.append((END_ELEMENT, name, self.getpos()[0]))
+        line_number = self.getpos()[0]
+
+        # Discard lonely end tags
+        index = len(self.stack) - 1
+        while index >= 0 and self.stack[index] != name:
+            index = index - 1
+
+        if index < 0:
+            # XXX Better to log it
+##            warnings.warn('discarding unexpected "</%s>" at line %s'
+##                          % (name, line_number))
+            return
+
+        element_name = self.stack.pop()
+        # Close missing optional end tags
+        while name != element_name:
+            if element_name in optional_end_tag_elements:
+                element_name = self.stack.pop()
+                self.events.append((END_ELEMENT, element_name, line_number))
+            else:
+                raise ValueError, 'missing end tag </%s>' % element_name
+
+        self.events.append((END_ELEMENT, name, line_number))
 
 
     def handle_comment(self, data):

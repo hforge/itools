3d65bd6bfcaab46eb29489ea237fad350322b4e7
J. David Ibanez <jdavid@itaapy.com>
2005-07-22 12:17:35 +0000

    handlers, implement "has_handler" for files
    git-archimport-id: jdavid@itaapy.com--public/itools--main--0.9--patch-102

--- handlers/Handler.py
+++ handlers/Handler.py
@@ -89,6 +89,27 @@ class Node(object):
         return self.parent.acquire(name)
 
 
+    def _get_handler_names(self):
+        return []
+
+
+    def get_handler_names(self, path='.'):
+        container = self.get_handler(path)
+        return container._get_handler_names()
+
+
+    def has_handler(self, path):
+        # Normalize the path
+        if not isinstance(path, uri.Path):
+            path = uri.Path(path)
+
+        path, segment = path[:-1], path[-1]
+        name = segment.name
+
+        container = self.get_handler(path)
+        return name in container.get_handler_names()
+
+
     def get_handler(self, path):
         # Be sure path is a Path
         if not isinstance(path, uri.Path):

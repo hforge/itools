3bf457aaf6ed9ab825454b340d3cbfbb45feea39
J. David Ibanez <jdavid@itaapy.com>
2006-03-15 22:34:05 +0100

    [packaging] Rename configuration file to "setup.conf". Move setup code to the function "itools.setup.setup", so it can be reused in other packages.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 # Import from the Standard Library
-from distutils.core import setup
+from distutils import core
 import os
 
 # Import from itools
@@ -32,10 +32,6 @@ from handlers.config import Config
 end_local_import()
 # END HACK
 ############################################################################
-
-
-
-config = Config(get_resource('config'))
 
 
 description = """itools is a Python library, it groups a number of packages
@@ -64,74 +60,85 @@ included are:
 """
 
 
-# The package name
-package_name = config.get_value('name')
-
-# The list of sub-packages
-subpackages = config.get_value('packages').split()
-
-# The data in the packages
-packages = [package_name]
-package_data = {package_name: []}
-for subpackage_name in subpackages:
-    packages.append('%s.%s' % (package_name, subpackage_name))
-
-
-if not os.path.exists('MANIFEST'):
-    os.system('git-ls-files > MANIFEST')
-
-for line in open('MANIFEST').readlines():
-    line = line.strip()
-    # Python files are included by default
-    if line.endswith('.py'):
-        continue
-
-    path = line.split('/')
-    n = len(path)
-    if n == 1:
-        package_data[package_name].append(line)
-    elif path[0] == 'locale':
-        package_data[package_name].append(line)
-    elif path[0] in subpackages:
-        files = package_data.setdefault('%s.%s' % (package_name, path[0]), [])
-        files.append(os.path.join(*path[1:]))
-
-# The scripts
-scripts = config.get_value('scripts').split()
-scripts = [ os.path.join(*['scripts', x]) for x in scripts ]
+classifiers = ['Development Status :: 3 - Alpha',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved :: GNU General Public License (GPL)',
+               'Programming Language :: Python',
+               'Topic :: Internet',
+               'Topic :: Internet :: WWW/HTTP',
+               'Topic :: Software Development',
+               'Topic :: Software Development :: Internationalization',
+               'Topic :: Software Development :: Libraries',
+               'Topic :: Software Development :: Libraries :: Python Modules',
+               'Topic :: Software Development :: Localization',
+               'Topic :: Text Processing',
+               'Topic :: Text Processing :: Markup',
+               'Topic :: Text Processing :: Markup :: XML'],
 
 
-setup(name = package_name,
-      version = __version__,
-      # Metadata
-      # XXX Broken distutils, "sdist" don't likes unicode strings, and
-      # "register" don't likes normal strings.
-      author = config.get_value('author_name'),
-      author_email = config.get_value('author_email'),
-      license = config.get_value('license'),
-      url = config.get_value('url'),
-      description = config.get_value('description'),
-      long_description = description,
-      classifiers = ['Development Status :: 3 - Alpha',
-                     'Intended Audience :: Developers',
-                     'License :: OSI Approved :: GNU General Public License (GPL)',
-                     'Programming Language :: Python',
-                     'Topic :: Internet',
-                     'Topic :: Internet :: WWW/HTTP',
-                     'Topic :: Software Development',
-                     'Topic :: Software Development :: Internationalization',
-                     'Topic :: Software Development :: Libraries',
-                     'Topic :: Software Development :: Libraries :: Python Modules',
-                     'Topic :: Software Development :: Localization',
-                     'Topic :: Text Processing',
-                     'Topic :: Text Processing :: Markup',
-                     'Topic :: Text Processing :: Markup :: XML'],
-      # Packages
-      package_dir = {package_name: ''},
-      packages = packages,
-      package_data = package_data,
-      # Scripts
-      scripts = scripts,
-      # XXX broken distutils
-      cmdclass={'build_py': build_py_fixed},
-      )
+
+def setup():
+    config = Config(get_resource('setup.conf'))
+
+    # The package name
+    package_name = config.get_value('name')
+
+    # The list of sub-packages
+    subpackages = config.get_value('packages').split()
+
+    # The data in the packages
+    packages = [package_name]
+    package_data = {package_name: []}
+    for subpackage_name in subpackages:
+        packages.append('%s.%s' % (package_name, subpackage_name))
+
+    # Write the manifest file if it does not exists
+    if not os.path.exists('MANIFEST'):
+        os.system('git-ls-files > MANIFEST')
+
+    # The data files
+    for line in open('MANIFEST').readlines():
+        line = line.strip()
+        # Python files are included by default
+        if line.endswith('.py'):
+            continue
+
+        path = line.split('/')
+        n = len(path)
+        if n == 1:
+            package_data[package_name].append(line)
+        elif path[0] == 'locale':
+            package_data[package_name].append(line)
+        elif path[0] in subpackages:
+            subpackage = '%s.%s' % (package_name, path[0])
+            files = package_data.setdefault(subpackage, [])
+            files.append(os.path.join(*path[1:]))
+
+    # The scripts
+    scripts = config.get_value('scripts').split()
+    scripts = [ os.path.join(*['scripts', x]) for x in scripts ]
+
+    core.setup(name = package_name,
+               version = __version__,
+               # Metadata
+               # XXX Broken distutils, "sdist" don't likes unicode strings,
+               # and "register" don't likes normal strings.
+               author = config.get_value('author_name'),
+               author_email = config.get_value('author_email'),
+               license = config.get_value('license'),
+               url = config.get_value('url'),
+               description = config.get_value('description'),
+               long_description = description,
+               classifiers = classifiers,
+               # Packages
+               package_dir = {package_name: ''},
+               packages = packages,
+               package_data = package_data,
+               # Scripts
+               scripts = scripts,
+               # XXX broken distutils
+               cmdclass={'build_py': build_py_fixed})
+
+
+if __name__ == '__main__':
+    setup()

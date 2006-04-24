0d11a3df66c9daa3bca4aa6a37a05a8aa1d3e5c6
Hervé Cauwelier <herve@itaapy.com>
2006-04-24 15:23:42 +0200

    [#305] move FolderWithRoot class into RoleAware mixin class. Not 100% clean but concerns are better separated. (cherry picked from e52f7054dcb3bbc98b791de39924b502523287e2 commit)

--- cms/Group.py
+++ cms/Group.py
@@ -16,7 +16,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 # Import from the Standard Library
-from time import time
+from operator import attrgetter
 
 # Import from itools
 from itools import handlers
@@ -29,7 +29,6 @@ from access import AccessControl
 from utils import comeback, checkid
 from widgets import Table
 from Folder import Folder
-from Handler import Handler
 
 
 
@@ -308,3 +307,183 @@ class Group(Folder):
 
 
 Folder.register_handler_class(Group)
+
+
+
+class RoleAware(object):
+
+    __roles__ = [
+        {'name': 'members', 'title': u"Members", 'unit': u"Member"},
+        {'name': 'reviewers', 'title': u"Reviewers", 'unit': u"Reviewer"},
+    ]
+
+
+    def get_roles(self):
+        return list(self.__roles__)
+
+
+    def get_role_names(self):
+        return [r['name'] for r in self.__roles__]
+
+
+    def get_skeleton(self, **kw):
+        skeleton = {}
+
+        # build roles
+        for role in self.get_roles():
+            rolename = role['name']
+            users = kw.get(rolename, [])
+            skeleton['.%s' % rolename] = ListOfUsers(users=users)
+
+        return skeleton
+
+
+    def get_handler(self, segment, resource):
+        return ListOfUsers(resource)
+
+
+    def is_in_role(self, rolename, username=None):
+        if username is None:
+            context = get_context()
+            user = context.user
+            if user is None:
+                return False
+            username = user.name
+
+        role = self.get_handler('.%s' % rolename)
+        return username in role.get_usernames()
+
+
+    def del_roles(self, username):
+        for role in self.get_roles():
+            handler = self.get_handler('.%s' % role['name'])
+            if username in handler.get_usernames():
+                handler.remove(username)
+
+        context = get_context()
+        root = context.root
+        user = root.get_user(username)
+        user.clear_group_cache()
+
+
+    def set_role(self, rolename, username):
+        for role in self.get_roles():
+            handler = self.get_handler('.%s' % role['name'])
+            if rolename == role['name']:
+                if not username in handler.get_usernames():
+                    handler.add(username)
+            else:
+                if username in handler.get_usernames():
+                    handler.remove(username)
+
+        context = get_context()
+        root = context.root
+        user = root.get_user(username)
+        user.clear_group_cache()
+
+
+    #########################################################################
+    # User Interface
+    def get_roles_namespace(self, username):
+        namespace = []
+
+        for role in self.get_roles():
+            rolename = role['name']
+            namespace.append({
+                'name': rolename,
+                'title': role['unit'],
+                'selected': self.is_in_role(rolename, username)
+            })
+
+        return namespace
+
+
+    def get_views(self):
+        return ['permissions_form']
+
+
+    def _get_members_sort_key(self):
+        return 'title_or_name'
+
+
+    permissions_form__access__ = 'is_allowed_to_edit'
+    permissions_form__label__ = u"Permissions"
+    def permissions_form(self):
+        context = get_context()
+        root = context.root
+        userfolder = root.get_handler('users')
+        namespace = {}
+
+        members = set()
+        for role in self.get_roles():
+            rolename = role['name']
+            handler = self.get_handler('.%s' % rolename)
+            members = members.union(handler.get_usernames())
+
+        users = [userfolder.get_handler(u) for u in members]
+        key = attrgetter(self._get_members_sort_key())
+        users.sort(key=key)
+        namespace['users'] = []
+
+        for user in users:
+            username = user.name
+            info = {}
+            info['name'] = username
+            info['title'] = user.get_title_or_name()
+            info['roles'] = self.get_roles_namespace(username)
+            namespace['users'].append(info)
+
+        others = []
+        for user in userfolder.search_handlers():
+            if user.name not in members:
+                others.append(user)
+        others.sort(key=key)
+
+        namespace['others'] = []
+        for user in others:
+            info = {}
+            info['name'] = user.name
+            info['title'] = user.get_title_or_name()
+            namespace['others'].append(info)
+
+        handler = self.get_handler('/ui/Folder_permissions.xml')
+        return stl(handler, namespace)
+
+
+    permissions__access__ = 'is_allowed_to_edit'
+    def permissions(self, delusers=[], addusers=[], **kw):
+        context = get_context()
+        root = context.root
+
+        # permissions to remove
+        if kw.get('delete'):
+            if isinstance(delusers, str):
+                delusers = [delusers]
+            for username in delusers:
+                self.del_roles(username)
+
+            message = u"Members deleted."
+            return comeback(message)
+
+        # permissions to add
+        elif kw.get('add'):
+            if isinstance(addusers, str):
+                addusers = [addusers]
+            for username in addusers:
+                self.set_role('members', username)
+
+            message = u"Members added."
+            return comeback(message)
+
+        # permissions to change
+        elif kw.get('update'):
+            userfolder = root.get_handler('users')
+            for username, new_role in kw.items():
+                if username in delusers or (
+                    username in addusers or (
+                        not userfolder.has_handler(username))):
+                    continue
+                self.set_role(new_role, username)
+
+            message = u"Roles updated."
+            return comeback(message)

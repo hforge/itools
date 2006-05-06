18d23f30d82002fd9e884211342b5b0a2e4c43ca
J. David Ibanez <jdavid@itaapy.com>
2006-05-06 20:16:57 +0200

    [handlers] Move the registry to its own module (everything broken now). (cherry picked from 35ef52ae5fbfab13d727fbfd8a1bfefd9473b60d commit)

---
+++ handlers/registry.py
@@ -0,0 +1,50 @@
+# -*- coding: ISO-8859-1 -*-
+# Copyright (C) 2006 Juan David Ibáñez Palomar <jdavid@itaapy.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+
+handler_classes = {}
+
+
+def register_handler_class(handler_class):
+    for mimetype in handler_class.class_mimetypes:
+        handler_classes[mimetype] = handler_class
+
+
+def get_handler_class(resource):
+    mimetype = resource.get_mimetype()
+    if mimetype is not None:
+        if mimetype in handler_classes:
+            return handler_classes[mimetype]
+
+        main_type = mimetype.split('/')[0]
+        if main_type in handler_classes:
+            return handler_classes[main_type]
+
+    if isinstance(resource, base.File):
+        from File import File
+        return File
+    elif isinstance(resource, base.Folder):
+        from Folder import Folder
+        return Folder
+
+    raise ValueError
+
+
+def build_handler(resource, **kw):
+    handler_class = get_handler_class(resource, **kw)
+    return handler_class(resource)

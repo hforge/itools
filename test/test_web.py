b63a0fdffdbc34f1a509c1a78f5be0530bd6eed3
J. David Ibanez <jdavid@itaapy.com>
2006-07-11 21:06:42 +0200

    [tests] Move tests for HTTPDate to "test_http". (cherry picked from c9a2037fa5c31bee7fc545077c73ad00d4fc31b8 commit)

--- test/test_web.py
+++ test/test_web.py
@@ -13,41 +13,12 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
 # Import from the Standard Library
-from datetime import datetime
 import unittest
 from unittest import TestCase
 
-# Import from itools
-from itools.web.headers import HTTPDate
-
-
-class DateTestCase(TestCase):
-
-    def test_rfc822(self):
-        date = 'Sun, 06 Nov 1994 08:49:37 GMT'
-        date = HTTPDate.decode(date)
-        self.assertEqual(date, datetime(1994, 11, 6, 8, 49, 37))
-
-
-    def test_rfc850(self):
-        date = 'Sunday, 06-Nov-94 08:49:37 GMT'
-        date = HTTPDate.decode(date)
-        self.assertEqual(date, datetime(1994, 11, 6, 8, 49, 37))
-
-
-    def test_asctime(self):
-        date = 'Sun Nov  6 08:49:37 1994'
-        date = HTTPDate.decode(date)
-        self.assertEqual(date, datetime(1994, 11, 6, 8, 49, 37))
-
-
-    def test_encode(self):
-        date = datetime(1994, 11, 6, 8, 49, 37)
-        date = HTTPDate.encode(date)
-        self.assertEqual(date, 'Sun, 06 Nov 1994 08:49:37 GMT')
 
 
 if __name__ == '__main__':

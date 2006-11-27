7404f7e83f01f05f84fa82ab4b9b9fcd644b8aa8
J. David Ibanez <jdavid@itaapy.com>
2006-11-27 19:50:25 +0100

    [csv] Fix unit tests (work in progress).

--- test/test_csv.py
+++ test/test_csv.py
@@ -22,8 +22,6 @@ import unittest
 from unittest import TestCase
 
 # Import from itools
-from itools.resources import memory
-from itools.resources import get_resource
 from itools.datatypes import Date, Integer, Unicode, URI
 from itools.catalog import queries
 from itools.csv import CSV
@@ -42,33 +40,34 @@ class CSVTestCase(TestCase):
 
     def test_unicode(self):
         data = '"Martin von Löwis","Marc André Lemburg","Guido van Rossum"\n'
-        resource = memory.File(data)
-        handler = CSV(resource)
+        handler = CSV()
+        handler.load_state_from_string(data)
         self.assertEqual(handler.get_rows(), [[u"Martin von Löwis",
                                                u"Marc André Lemburg",
                                                u"Guido van Rossum"]])
 
 
     def test_num_of_lines(self):
-        resource = memory.File(TEST_DATA_2)
-        handler = CSV(resource)
-        self.assertEqual(len(handler.get_rows()), 3)
+        handler = CSV()
+        handler.load_state_from_string(TEST_DATA_2)
+        rows = list(handler.get_rows())
+        self.assertEqual(len(rows), 3)
 
 
     def test_num_of_lines_with_last_new_line(self):
         data = TEST_DATA_2 + '\r\n'
-        resource = memory.File(data)
-        handler = CSV(resource)
-        self.assertEqual(len(handler.get_rows()), 3)
+        handler = CSV()
+        handler.load_state_from_string(data)
+        rows = list(handler.get_rows())
+        self.assertEqual(len(rows), 3)
 
     
     def test_load_state_with_schema(self):
-        resource = memory.File(TEST_DATA_1)
         handler = CSV()
         handler.columns = ['name', 'url', 'number', 'date']
         handler.schema = {'name': Unicode, 'url': URI, 'number': Integer,
                           'date': Date}
-        handler.load_state(resource)
+        handler.load_state_from_string(TEST_DATA_1)
         self.assertEqual(handler.get_rows(), [
             [u"python", URI.decode('http://python.org'), 52343,
              Date.decode('2003-10-23')], 
@@ -77,23 +76,21 @@ class CSVTestCase(TestCase):
 
 
     def test_load_state_without_schema(self):
-        resource = memory.File(TEST_DATA_1)
         handler = CSV()
         handler.schema = {'name': Unicode, 'url': URI, 'number': Integer,
                           'date': Date}
-        handler.load_state(resource)
+        handler.load_state_from_string(TEST_DATA_1)
         self.assertEqual(handler.get_rows(), [
             [u"python", u'http://python.org', u'52343', u'2003-10-23'], 
             [u"ruby", u'http://ruby-lang.org', u'42352', u'2001-03-28']])
 
 
     def test_to_str_with_schema(self):
-        resource = memory.File(TEST_DATA_1)
         handler = CSV()
         handler.columns = ['name', 'url', 'number', 'date']
         handler.schema = {'name': Unicode, 'url': URI, 'number': Integer,
                           'date': Date}
-        handler.load_state(resource)
+        handler.load_state_from_string(TEST_DATA_1)
         self.assertEqual(
             handler.to_str(),
             u'"python","http://python.org","52343","2003-10-23"\n'
@@ -101,9 +98,8 @@ class CSVTestCase(TestCase):
 
 
     def test_to_str_without_schema(self):
-        resource = memory.File(TEST_DATA_1)
         handler = CSV()
-        handler.load_state(resource)
+        handler.load_state_from_string(TEST_DATA_1)
         self.assertEqual(
             handler.to_str(),
             u'"python","http://python.org","52343","2003-10-23"\n'
@@ -111,75 +107,71 @@ class CSVTestCase(TestCase):
 
 
     def test_get_row(self):
-        resource = memory.File(TEST_DATA_2)
-        handler = CSV(resource)
+        handler = CSV()
+        handler.load_state_from_string(TEST_DATA_2)
         self.assertEqual(handler.get_row(1), ['four', 'five', 'six'])
 
 
     def test_get_rows(self):
-        resource = memory.File(TEST_DATA_2)
-        handler = CSV(resource)
+        handler = CSV()
+        handler.load_state_from_string(TEST_DATA_2)
         self.assertEqual(handler.get_rows([0, 1]), [
             ['one', 'two', 'three'], 
             ['four', 'five', 'six']])
 
 
     def test_add_row(self):
-        resource = memory.File(TEST_DATA_2)
-        handler = CSV(resource)
+        handler = CSV()
+        handler.load_state_from_string(TEST_DATA_2)
         handler.add_row(['a', 'b', 'c'])
         self.assertEqual(handler.get_row(3), ['a', 'b', 'c'])
 
 
     def test_del_row(self):
-        resource = memory.File(TEST_DATA_2)
-        handler = CSV(resource)
+        handler = CSV()
+        handler.load_state_from_string(TEST_DATA_2)
         handler.del_row(1)
         self.assertEqual(handler.get_row(1), ['seven', 'eight', 'nine'])
 
 
     def test_del_rows(self):
-        resource = memory.File(TEST_DATA_2)
-        handler = CSV(resource)
+        handler = CSV()
+        handler.load_state_from_string(TEST_DATA_2)
         handler.del_rows([0, 1])
         self.assertEqual(handler.get_row(0), ['seven', 'eight', 'nine'])
 
 
     def test_set_state_in_memory_resource(self):
-        resource = memory.File(TEST_DATA_2)
-        handler = CSV(resource)
+        handler = CSV()
+        handler.load_state_from_string(TEST_DATA_2)
         handler.add_row(['a', 'b', 'c'])
         handler.save_state()
-        handler2 = CSV(resource)
+        handler2 = CSV()
+        handler2.load_state_from_string(TEST_DATA_2)
         self.assertEqual(handler2.get_row(3), ['a', 'b', 'c'])
 
 
     def test_set_state_in_file_resource(self):
-        resource = get_resource('test.csv')
-        handler = CSV(resource)
+        handler = CSV('test.csv')
         handler.add_row(['d1', 'e1', 'f1'])
         handler.save_state()
 
-        resource2 = get_resource('test.csv')
-        handler2 = CSV(resource2)
+        handler2 = CSV('test.csv')
         self.assertEqual(handler2.get_row(3), ['d1', 'e1', 'f1'])
         handler2.del_row(3)
         handler2.save_state()
 
-        resource = get_resource('test.csv')
-        handler = CSV(resource)
+        handler = CSV('test.csv')
         self.assertEqual(handler.get_nrows(), 3)
 
 
     def test_indexes_hit_in_one_row(self):
-        data = TEST_DATA_1
-        resource = memory.File(data)
         handler = CSV()
         handler.columns = ['name', 'url', 'number', 'date']
         handler.schema = {'name': Unicode, 'url': URI,
-                          'number': Integer(index=True),
-                          'date': Date(index=True)}
-        handler.load_state(resource)
+                          'number': Integer(index='keyword'),
+                          'date': Date(index='keyword')}
+        handler.load_state_from_string(TEST_DATA_1)
         self.assertEqual(len(handler.indexes), 4)
         self.assertEqual(handler.search(number=52343), [0])
         self.assertEqual(handler.search(date=Date.decode('2001-03-28')), [1])
@@ -187,34 +179,34 @@ class CSVTestCase(TestCase):
 
     def test_indexes_hit_in_many_rows(self):
         data = 'house,2005-10-10\nwindow,2005-05-10\ncomputer,2005-10-10'
-        resource = memory.File(data)
         handler = CSV()
+        handler.load_state_from_string(data)
         handler.columns = ['name', 'date']
-        handler.schema = {'name': Unicode, 'date': Date(index=True)}
-        handler.load_state(resource)
+        handler.schema = {'name': Unicode, 'date': Date(index='keyword')}
+        handler.load_state_from_string(data)
         self.assertEqual(handler.search(date=Date.decode('2005-01-01')), [])
         self.assertEqual(handler.search(date=Date.decode('2005-10-10')), [0,2])
 
 
     def test_index_new_row(self):
         data = 'house,2005-10-10\nwindow,2005-05-10\ncomputer,2005-10-10'
-        resource = memory.File(data)
         handler = CSV()
+        handler.load_state_from_string(data)
         handler.columns = ['name', 'date']
-        handler.schema = {'name': Unicode, 'date': Date(index=True)}
-        handler.load_state(resource)
+        handler.schema = {'name': Unicode, 'date': Date(index='keyword')}
+        handler.load_state_from_string(data)
         handler.add_row(['flower', Date.decode('2005-05-10')])
         self.assertEqual(handler.search(date=Date.decode('2005-05-10')), [1,3])
 
 
     def test_index_del_row(self):
         data = 'house,2005-10-10\nwindow,2005-05-10\ncomputer,2005-10-10'
-        resource = memory.File(data)
         handler = CSV()
+        handler.load_state_from_string(data)
         handler.columns = ['name', 'date']
-        handler.schema = {'name': Unicode(index=True),
-                          'date': Date(index=True)}
-        handler.load_state(resource)
+        handler.schema = {'name': Unicode(index='text'),
+                          'date': Date(index='keyword')}
+        handler.load_state_from_string(data)
         self.assertEqual(handler.search(name='window'), [1])
         handler.del_row(1)
         self.assertEqual(handler.search(name='window'), [])
@@ -224,12 +216,11 @@ class CSVTestCase(TestCase):
 
 
     def test_build_csv_data(self):
-        resource = memory.File('')
         handler = CSV()
         handler.columns = ['name', 'surname', 'date']
-        handler.schema = {'name': Unicode, 'surname': Unicode(index=True),
-                          'date': Date(index=True)}
-        handler.load_state(resource)
+        handler.schema = {'name': Unicode, 'surname': Unicode(index='text'),
+                          'date': Date(index='keyword')}
+        handler.load_state_from_string('')
         handler.add_row(['Piotr', 'Macuk', '1975-12-08'])
         handler.add_row(['Basia', 'Macuk', '2002-02-14'])
         self.assertEqual(handler.search(surname='Macuk'), [0, 1])
@@ -242,13 +233,12 @@ class CSVTestCase(TestCase):
 
 
     def test_advanced_search(self):
-        resource = get_resource('test_adv.csv')
         handler = CSV()
         handler.columns = ['id', 'name', 'country', 'date']
-        handler.schema = {'id': Integer, 'name': Unicode(index=True),
-                          'country': Unicode(index=True),
-                          'date': Date(index=True)}
-        handler.load_state(resource)
+        handler.schema = {'id': Integer, 'name': Unicode(index='text'),
+                          'country': Unicode(index='text'),
+                          'date': Date(index='text')}
+        handler.load_state_from_file('test_adv.csv')
         result1 = handler.search(name='dde', country='Sweden')
         self.assertEqual(result1, [5, 6])
 
@@ -270,24 +260,22 @@ class CSVTestCase(TestCase):
 
 
     def test_access_by_name(self):
-        resource = memory.File(TEST_DATA_1)
         handler = CSV()
         handler.columns = ['name', 'url', 'number', 'date']
         handler.schema = {'name': Unicode, 'url': URI, 'number': Integer,
                           'date': Date}
-        handler.load_state(resource)
+        handler.load_state_from_string(TEST_DATA_1)
 
         row = handler.get_row(1)
         self.assertEqual(row.get_value('name'), u'ruby')
 
 
     def test_get_row_as_handler(self):
-        resource = memory.File(TEST_DATA_1)
         handler = CSV()
         handler.columns = ['name', 'url', 'number', 'date']
         handler.schema = {'name': Unicode, 'url': URI, 'number': Integer,
                           'date': Date}
-        handler.load_state(resource)
+        handler.load_state_from_string(TEST_DATA_1)
 
         row = handler.get_handler('0')
         self.assertEqual(row.get_value('name'), u'python')
@@ -295,16 +283,16 @@ class CSVTestCase(TestCase):
 
 
     def test_bad_syntax_csv_file(self):
-        resource = memory.File(TEST_SYNTAX_ERROR)
-        self.assertRaises(ValueError, CSV, resource)
+        load_state = CSV().load_state_from_string
+        self.assertRaises(ValueError, load_state, TEST_SYNTAX_ERROR)
 
 
     def test_bad_syntax_csv_file_with_schema(self):
-        resource = memory.File(TEST_SYNTAX_ERROR)
         handler = CSV()
         handler.columns = ['one', 'two', 'three']
         handler.schema = {'one': Unicode, 'two': Unicode, 'three': Unicode}
-        self.assertRaises(ValueError, handler.load_state, resource)
+        load_state = handler.load_state_from_string
+        self.assertRaises(ValueError, load_state, TEST_SYNTAX_ERROR)
 
 
 

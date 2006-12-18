79fdd99a6822132bb229dfa60884a29ae3674140
J. David Ibanez <jdavid@itaapy.com>
2006-12-18 16:11:51 +0100

    [cms] Get upgrade working.

--- cms/root.py
+++ cms/root.py
@@ -484,6 +484,9 @@ class Root(WebSite):
             user.set_property('ikaaro:username', name)
             i += 1
 
+        # Save
+        get_transaction().commit()
+
         # Update 'members' index
         self._update_catalog()
 

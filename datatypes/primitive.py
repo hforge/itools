f3d574622899a71f33cb2bdebfe8d2b9b468392c
J. David Ibanez <jdavid@itaapy.com>
2007-01-09 10:08:37 +0100

    [datatypes] Import the "re" module.

--- datatypes/primitive.py
+++ datatypes/primitive.py
@@ -13,12 +13,13 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
 # Import from the Standard Library
 import datetime
-import mimetypes
 import decimal
+import mimetypes
+import re
 
 # Import from itools
 from itools.uri import get_reference

27b9a436c5be8b4bfb25a55513a483d2307a4a74
J. David Ibanez <jdavid@itaapy.com>
2007-01-15 20:07:36 +0100

    [cms] Fix copyright notices in the skeleton package.

--- cms/skeleton/__init__.py
+++ cms/skeleton/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright (C) 2004-2005 Luis Belmar-Letelier <luis@itaapy.com>
-#               2006 Hervé Cauwelier <herve@itaapy.com>
+# Copyright (C) YEAR(S) AUTHOR_NAME <EMAIL@ADDRESS>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -14,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
 # Import from itools
 from itools import get_abspath, get_version
--- cms/skeleton/setup.py
+++ cms/skeleton/setup.py
@@ -1,6 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright (C) 2004-2006 Luis Belmar-Letelier <luis@itaapy.com>
-#               2006 Hervé Cauwelier <herve@oursours.net>
+# Copyright (C) YEAR(S) AUTHOR_NAME <EMAIL@ADDRESS>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License

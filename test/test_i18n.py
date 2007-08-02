d898dd01a71a08fbc0dd834d3aca582688acb29b
J. David Ibanez <jdavid@itaapy.com>
2007-08-02 18:06:50 +0200

    Remove very old copyright, start from 2002.

--- test/test_i18n.py
+++ test/test_i18n.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright (C) 2001-2002, 2006-2007 Juan David Ibáñez Palomar <jdavid@itaapy.com>
+# Copyright (C) 2002, 2006-2007 Juan David Ibáñez Palomar <jdavid@itaapy.com>
 # Copyright (C) 2004 J. Thierry Fromon <from.t@free.fr>
 #
 # This program is free software: you can redistribute it and/or modify

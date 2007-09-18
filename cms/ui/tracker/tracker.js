d313c63beb4d1a05cda4aef92734fc495e97c460
Nicolas Deram <nicolas@itaapy.com>
2007-09-18 11:15:31 +0200

    itools.cms: Tracker: improve display by replacing html inserted from

--- cms/ui/tracker/tracker.js
+++ cms/ui/tracker/tracker.js
@@ -1,5 +1,20 @@
 function reply(id){
   var comment = document.getElementById('comment'+id).innerHTML;
+
+  /* Replace html by text */
+  var reg = new RegExp("(&gt;)", "g");
+  comment = comment.replace(reg, '>');
+  reg = new RegExp("(&lt;)", "g");
+  comment = comment.replace(reg, '<');
+  reg = new RegExp("(&nbsp;)", "g");
+  comment = comment.replace(reg, ' ');
+  reg1 = new RegExp('(<a href=")', "g");
+  reg2 = new RegExp('(">.*</a>)', "g");
+  if (comment.match(reg1) && comment.match(reg2)) {
+      comment = comment.replace(reg1, '');
+      comment = comment.replace(reg2, '');
+  }
+
   comment = comment.split(/\r|\n/);
   replytext = ''
   for (var i=0; i < comment.length; i++){

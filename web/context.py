2511bd3dc7f04851e08aa7128cace0782e4bd6ca
Taverne Sylvain <taverne.sylvain@gmail.com>
2008-01-15 20:46:22 +0100

    Fix a small bug in web.context. (keep is True)

--- web/context.py
+++ web/context.py
@@ -206,7 +206,7 @@ class Context(object):
             if isinstance(value, tuple) and len(value) == 3:
                 continue
             # Keep form field
-            if (key is True) or (key in keep):
+            if (keep is True) or (key in keep):
                 form[key] = value
         if form:
             goto = goto.replace(**form)

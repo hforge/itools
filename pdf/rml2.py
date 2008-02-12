20fd928bec432beecf5a6e98f6bc76ae937660d6
Yannick Martel <yannick.martel@gmail.com>
2008-02-12 17:38:11 +0100

    Add:
    - ordonned list support
    - h1~h3 support
    - hr support
    Fix the alignment attribute on <p> tag

--- pdf/rml2.py
+++ pdf/rml2.py
@@ -10,9 +10,14 @@ from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.units import inch, cm, mm, pica
 from reportlab.lib.pagesizes import (letter, legal, elevenSeventeen, A0, A1,
     A2, A3, A4, A5, A6, B0, B1, B2, B3, B4, B5, B6, landscape, portrait)
-from reportlab.platypus import (Paragraph, SimpleDocTemplate, Preformatted)
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.enums import TA_LEFT, TA_CENTER, TA_RIGHT, TA_JUSTIFY
+from reportlab.platypus import (Paragraph, SimpleDocTemplate, Preformatted)
+from reportlab.platypus.flowables import Flowable, HRFlowable
 
+
+__tab_para_alignment = {'LEFT': TA_LEFT, 'RIGHT': TA_RIGHT,
+                        'CENTER': TA_CENTER, 'JUSTIFY': TA_JUSTIFY}
 TAG_NOT_SUPPORTED = '%s: line %s tag "%s" is currently not supported.'
 WARNING_DTD = '%s: line %s tag "%s" is unapproprieted here.'
 encoding = 'UTF-8'
@@ -42,7 +47,7 @@ def document_stream(stream, pdf_stream, document_name, is_test=False):
 #    pdf_table_style = {}
     # tag alias
     # Aliases allow you to assign more than one name to a paragraph style.
-#    alias_style = {}
+    alias_style = {}
 #    page_templates = []
     stack = []
     story = []
@@ -58,8 +63,8 @@ def document_stream(stream, pdf_stream, document_name, is_test=False):
             elif tag_name == 'head':
                 a = 2
             elif tag_name == 'body':
-                story += body_stream(stream, tag_uri, tag_name, attributes)
-
+                story += body_stream(stream, tag_uri, tag_name, attributes,
+                                     alias_style)
             else :
                 print TAG_NOT_SUPPORTED % ('document', line_number, tag_name)
                 # unknown tag
@@ -91,7 +96,7 @@ def document_stream(stream, pdf_stream, document_name, is_test=False):
         return (_story, pdf_stylesheet)
 
 
-def body_stream(stream, _tag_uri, _tag_name, _attributes):
+def body_stream(stream, _tag_uri, _tag_name, _attributes, alias_style):
     """
         stream : parser stream
     """
@@ -110,8 +115,13 @@ def body_stream(stream, _tag_uri, _tag_name, _attributes):
             tag_uri, tag_name, attributes = value
             if tag_name == 'p':
                 story.append(p_stream(stream, tag_uri, tag_name, attributes, pdf_stylesheet))
-            if tag_name == 'pre':
+            elif tag_name == 'pre':
                 story.append(pre_stream(stream, tag_uri, tag_name, attributes, pdf_stylesheet))
+            elif tag_name in ['h1','h2','h3']:
+                story.append(heading_stream(stream, tag_uri, tag_name,
+                            attributes, pdf_stylesheet, alias_style))
+            elif tag_name == 'hr':
+                story.append(hr_stream(stream, tag_uri, tag_name, attributes))
             else:
                 print TAG_NOT_SUPPORTED % ('document', line_number, tag_name)
                 # unknown tag
@@ -122,8 +132,10 @@ def body_stream(stream, _tag_uri, _tag_name, _attributes):
             tag_uri, tag_name = value
             if tag_name == 'body':
                 break
-
-    #return 1
+            else:
+                print TAG_NOT_SUPPORTED % ('document', line_number, tag_name)
+                # unknown tag
+                stack.append((tag_name, attributes, None))   
     return story
 
 
@@ -155,6 +167,10 @@ def p_stream(stream , tag_uri, tag_name, attributes, pdf_stylesheet):
                 content.append("<super>")
             elif tag_name == 'sub':
                 content.append("<sub>")
+            elif tag_name == 'br':
+                continue
+            elif tag_name == 'ol':
+                content += ol_stream(stream, tag_uri, tag_name, attributes, pdf_stylesheet)
             else:
                 print TAG_NOT_SUPPORTED % ('document', line_number, tag_name)
                 # unknown tag
@@ -175,6 +191,8 @@ def p_stream(stream , tag_uri, tag_name, attributes, pdf_stylesheet):
                 content.append("</super>")
             elif tag_name == 'sub':
                 content.append("</sub>")
+            elif tag_name == 'br':
+                content.append("<br/>")
             else:
                 print TAG_NOT_SUPPORTED % ('document', line_number, tag_name)
                 # unknown tag
@@ -208,7 +226,7 @@ def pre_stream(stream , tag_uri, tag_name, attributes, pdf_stylesheet):
             break
         #### START ELEMENT ####
         if event == START_ELEMENT:
-            print WARNING-DTD % ('document', line_number, tag_name)
+            print WARNING_DTD % ('document', line_number, tag_name)
             stack.append((tag_name, attributes, None))   
 
         #### END ELEMENT ####
@@ -227,6 +245,136 @@ def pre_stream(stream , tag_uri, tag_name, attributes, pdf_stylesheet):
                 # we dont strip the string --> preformatted widget
                 value = XML.encode(Unicode.decode(value, encoding)) # entities
                 content.append(value)
+
+
+def heading_stream(stream, _tag_uri, _tag_name, _attributes, pdf_stylesheet,
+                   alias_style):
+    """
+        Create a heading widget.
+    """
+
+    content = []
+    stack = []
+    stack.append((_tag_name, _attributes, None))
+    style = get_style(pdf_stylesheet, alias_style,  _tag_name)
+
+    while True:
+        event, value, line_number = stream_next(stream)
+        if event == None:
+            break
+        #### START ELEMENT ####
+        if event == START_ELEMENT:
+            tag_uri, tag_name, attributes = value
+            print WARNING_DTD % ('document', line_number, tag_name)
+            stack.append((tag_name, attributes, None))
+        #### END ELEMENT ####
+        elif event == END_ELEMENT:
+            tag_uri, tag_name = value
+            if tag_name == _tag_name:
+                content = ''.join(content)
+                widget = Paragraph(content, style)
+                return widget
+            else:
+                print WARNING_DTD % ('document', line_number, tag_name)
+                element = stack.pop()
+
+        #### TEXT ELEMENT ####
+        elif event == TEXT:
+            if stack:
+                value = normalize(Unicode.decode(value, encoding), True)
+                if len(value) > 0 and value != ' ':
+                    value = XML.encode(value) # entities
+                content.append(value)
+                has_content = True
+
+
+def hr_stream(stream, _tag_uri, _tag_name, _attributes):
+    """
+        Create a hr widget.
+    """
+
+    stack = []
+    stack.append((_tag_name, _attributes, None))
+    while True:
+        event, value, line_number = stream_next(stream)
+        if event == None:
+            break
+        #### START ELEMENT ####
+        if event == START_ELEMENT:
+            tag_uri, tag_name, attributes = value
+            stack.append((tag_name, attributes, None))
+        #### END ELEMENT ####
+        elif event == END_ELEMENT:
+            tag_uri, tag_name = value
+            if tag_name == _tag_name:
+                widget = create_hr(_attributes)
+                return widget
+            else:
+                element = stack.pop()
+        #### TEXT ELEMENT ####
+        elif event == TEXT:
+            pass
+
+
+def ol_stream(stream , tag_uri, tag_name, attributes, pdf_stylesheet):
+    #TODO Fix the space at the begining of a new list-lign
+    stack = []
+    story = []
+    styles = getSampleStyleSheet()
+    styleN = styles['Normal']
+    content = []
+    has_content = False
+    stack.append((tag_name, attributes, None))
+
+    while True:
+        event, value, line_number = stream_next(stream)
+        if event == None:
+            break
+        #### START ELEMENT ####
+        if event == START_ELEMENT:
+            tag_uri, tag_name, attributes = value
+            if tag_name == 'ol':
+                print WARNING_DTD % ('document', line_number, tag_name)
+            elif tag_name == 'li':
+                content.append("<seq> ")
+            else:
+                print TAG_NOT_SUPPORTED % ('document', line_number, tag_name)
+                # unknown tag
+                stack.append((tag_name, attributes, None))   
+
+        #### END ELEMENT ####
+        elif event == END_ELEMENT:
+            tag_uri, tag_name = value
+            if tag_name == 'ol':
+                return content
+            elif tag_name in ['i', 'em']:
+                content.append("</i>")
+            elif tag_name in ['b', 'strong']:
+                content.append("</b>")
+            elif tag_name == 'u':
+                content.append("</u>")
+            elif tag_name == 'sup':
+                content.append("</super>")
+            elif tag_name == 'sub':
+                content.append("</sub>")
+            elif tag_name == 'li':
+                content.append("</seq><br/>")
+            else:
+                print TAG_NOT_SUPPORTED % ('document', line_number, tag_name)
+                # unknown tag
+                stack.append((tag_name, attributes, None))   
+
+        #### TEXT ELEMENT ####        
+        elif event == TEXT:
+            value = normalize(Unicode.decode(value, encoding), True)
+            if len(value) > 0:
+                # alow to write :
+                # <para><u><i>foo</i> </u></para>
+                value = XML.encode(value) # entities
+                if has_content and content[-1] == '</br>':
+                    value = value.lstrip()
+                content.append(value)
+                has_content = True
 
 
 #######################################################################
@@ -311,6 +459,51 @@ def create_preformatted(pdf_stylesheet, alias_style, element, content):
     return widget
 
 
+def build_start_tag(tag_name, attributes):
+    """
+        Create the XML start tag from his name and his attributes
+    """
+
+    attr_str = ''.join([' %s="%s"' % (key[1], attributes[key])
+                        for key in attributes.keys()])
+    return '<%s%s>' % (tag_name, attr_str)
+
+
+def build_end_tag(tag_name):
+    """
+        Create the XML end tag from his name.
+    """
+
+    return '</%s>' % tag_name
+
+
+def create_hr(attributes):
+    """
+        Create a reportlab hr widget
+    """
+
+    attrs = {}
+    for key in ['width', 'thickness', 'spaceBefore', 'spaceAfter']:
+        if exist_attribute(attributes, [key]):
+            attrs[key] = rml_value(attributes.get((None, key)))
+    if exist_attribute(attributes, ['lineCap']):
+        line_cap = attributes.get((None,'lineCap'))
+        if line_cap not in ['butt', 'round', 'square']:
+            line_cap = 'butt'
+        attrs['lineCap'] = line_cap
+    if exist_attribute(attributes, ['color']):
+        attrs['color'] = get_color(attributes.get((None, 'color')))
+    if exist_attribute(attributes, ['align']):
+        hAlign = attributes.get((None, 'align'), '').upper()
+        if hAlign in ['LEFT', 'RIGHT', 'CENTER', 'CENTRE']:
+            attrs['hAlign'] = hAlign
+    if exist_attribute(attributes, ['vAlign']):
+        vAlign = attributes.get((None, 'vAlign'), '').upper()
+        if vAlign in ['TOP', 'MIDDLE', 'BOTTOM']:
+            attrs['vAlign'] = vAlign
+    return HRFlowable(**attrs)
+
+
 def get_style(stylesheet, alias, name):
     """
        Return the style corresponding to name or the style normal if it does
@@ -331,5 +524,17 @@ def get_style(stylesheet, alias, name):
             return style
         except KeyError:
             return stylesheet['Normal']
+
+
+def exist_attribute(attrs, keys):
+    """
+        Return True if all key in keys
+        are contained in the dictionnary attrs
+    """
+
+    for key in keys:
+        if attrs.has_key((None, key)) == False:
+            return False
+    return True
 
 

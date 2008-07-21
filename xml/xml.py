b50a9dc0fa878b767e43f475667b21b0bbade3ed
J. David Ibanez <jdavid@itaapy.com>
2008-07-21 10:54:16 +0200

    xml: fix "stream_to_str" when there is a doctype.

--- xml/xml.py
+++ xml/xml.py
@@ -114,15 +114,7 @@ def stream_to_str(stream, encoding='UTF-8'):
                     '<?xml version="%s" encoding="%s" standalone="%s"?>'
                     % (version, encoding, standalone))
         elif event == DOCUMENT_TYPE:
-            name, system_id, public_id, has_internal_subset = value
-            if public_id is None:
-                data.append('<!DOCTYPE %s SYSTEM "%s">' % (name, system_id))
-            elif system_id is None:
-                # XXX Special case for HTML (not good XML)
-                data.append('<!DOCTYPE %s PUBLIC "%s">' % (name, public_id))
-            else:
-                data.append('<!DOCTYPE %s PUBLIC "%s"\n  "%s">'
-                    % (name, public_id, system_id))
+            data.append(value)
         elif event == CDATA:
             data.append('<![CDATA[%s]]>' % value)
         else:

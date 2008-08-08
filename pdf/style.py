9d1be1c12d74b2db512d842d0b09623b117aa4c0
Dumont Sébastien <sebastien.dumont@itaapy.com>
2008-08-08 13:06:29 +0200

    rml2: improve style

--- pdf/style.py
+++ pdf/style.py
@@ -22,32 +22,42 @@
 from reportlab.lib.enums import TA_LEFT, TA_CENTER, TA_RIGHT, TA_JUSTIFY
 from reportlab.lib.styles import ParagraphStyle
 from reportlab.lib.units import cm
-from utils import FONT, font_value, format_size, get_color_as_hexa
+from utils import (FONT, font_value, format_size, get_color_as_hexa)
 
-ALIGNMENTS = {'left': TA_LEFT, 'right': TA_RIGHT, 'center': TA_CENTER,
+P_ALIGNMENTS = {'left': TA_LEFT, 'right': TA_RIGHT, 'center': TA_CENTER,
               'justify': TA_JUSTIFY}
 
-PADDINGS = {'padding-top' : 'spaceBefore', 'padding-bottom': 'spaceAfter',
-            'padding-left': 'leftIndent', 'padding-right': 'rightIndent'}
+P_PADDINGS = {'padding-top' : 'spaceBefore', 'padding-bottom': 'spaceAfter',
+              'padding-left': 'leftIndent', 'padding-right': 'rightIndent'}
 
 HEADING = ('h1', 'h2', 'h3', 'h4', 'h5', 'h6')
 
-def border_style(context, value):
-    tab = value.split()
+
+def p_border_style(key, value):
     style_attrs = {}
-    for i in tab:
-        size = format_size(i, None)
-        if size:
-            style_attrs['borderWidth'] = size
-            continue
-        color = get_color_as_hexa(i, None)
+    if key == 'border':
+        tab = value.split()
+        for element in tab:
+            size = format_size(element, None)
+            if size:
+                style_attrs['borderWidth'] = size
+                continue
+            color = get_color_as_hexa(element, None)
+            if color:
+                style_attrs['borderColor'] = color
+                continue
+    elif key == 'border-color':
+        color = get_color_as_hexa(value, None)
         if color:
             style_attrs['borderColor'] = color
-            continue
+    elif key == 'border-width':
+        size = format_size(value, None)
+        if size:
+            style_attrs['borderWidth'] = size
     return style_attrs
 
 
-def font_style(context, key, value):
+def p_font_style(key, value):
     style_attr = {}
     if key == 'font-family':
         style_attr['fontName'] = FONT.get(value, 'Helvetica')
@@ -56,15 +66,15 @@ def font_style(context, key, value):
     return style_attr
 
 
-def padding_style(context, key, value):
+def p_padding_style(key, value):
     style_attr = {}
-    value = format_size(value, None)
+    size = format_size(value, None)
     if value:
         if key == 'padding':
-            for attr_key in PADDINGS.values():
-                style_attr[attr_key] = value
-        elif key in PADDINGS.keys():
-            style_attr[PADDINGS[key]] = value
+            for padding in P_PADDINGS.values():
+                style_attr[padding] = size
+        elif key in P_PADDINGS.keys():
+            style_attr[P_PADDINGS[key]] = size
     return style_attr
 
 
@@ -74,19 +84,21 @@ def build_paragraph_style(context, element, style_css):
     parent_style_name = 'Normal'
     bulletText = None
 
+    style_attr['autoLeading'] = 'max'
+
     #FIXME must be moved in default css
     style_attr['spaceAfter'] = 0.2 * cm
-    for key in style_css.keys():
+    for key, value in style_css.iteritems():
         if key == 'color':
-            style_attr['textColor'] = get_color_as_hexa(style_css[key])
+            style_attr['textColor'] = get_color_as_hexa(value)
         elif key in ('background', 'background-color'):
-            style_attr['backColor'] = get_color_as_hexa(style_css[key])
+            style_attr['backColor'] = get_color_as_hexa(value)
         elif key.startswith('border'):
-            style_attr.update(border_style(context, style_css[key]))
+            style_attr.update(p_border_style(key, value))
         elif key.startswith('font'):
-            style_attr.update(font_style(context, style_css[key]))
+            style_attr.update(p_font_style(key, value))
         elif key.startswith('padding'):
-             style_attr.update(padding_style(context, key, style_css[key]))
+            style_attr.update(p_padding_style(key, value))
 
     # Overload the attributes values
     for key, attr_value in element[1].iteritems():
@@ -98,11 +110,10 @@ def build_paragraph_style(context, element, style_css):
             bulletText = attr_value
         else:
             if key == 'align':
-                attr_value = ALIGNMENTS.get(attr_value, TA_LEFT)
+                attr_value = P_ALIGNMENTS.get(attr_value, TA_LEFT)
             elif key in ['leftIndent', 'rightIndent']:
                 attr_value = context.format_size(attr_value)
             style_attr[key] = attr_value
-    style_attr['autoLeading'] = 'max'
 
     if element[0] in HEADING + ('toctitle', ):
         parent_style_name = element[0]

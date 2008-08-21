433c606a33071b3d8e68fabe5ef29b04e435bc55
Gautier Hayoun <gautier.hayoun@itaapy.com>
2008-08-21 12:22:39 +0200

    isetup: added the missing database from previous patch

---
+++ isetup/packages_db.py
@@ -0,0 +1,9 @@
+PACKAGES_DB = {'Twisted Core': {'E': 'Twisted-*', 'M': 'twisted'},
+               'Twisted Web':  {'E': 'Twisted_Web*', 'M': 'twisted/web'},
+               'Reportlab':    {'E': 'Reportlab*', 'M': 'reportlab'},
+               'AptUrl':       {'E': 'apturl-*', 'M': 'AptUrl'},
+               'CodeHack':     {'E': 'CodeHack-*', 'M': 'codehack'},
+               'Onboard':      {'E': 'onboard-*', 'M': 'Onboard'},
+               'python-apt':   {'E': 'python_apt*', 'M': 'apt'},
+               'ccsm':         {'E': 'ccsm*', 'M': 'ccm'},
+               'pysqlite2':    {'E': 'pysqlite*', 'M': 'pysqlite2'}}

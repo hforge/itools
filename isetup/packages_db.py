1838b94a05ca26e619d4e71953b1e670300096bf
J. David Ibanez <jdavid@itaapy.com>
2008-08-21 12:43:04 +0200

    isetup: add "logilab" to the package database.

--- isetup/packages_db.py
+++ isetup/packages_db.py
@@ -1,9 +1,14 @@
-PACKAGES_DB = {'Twisted Core': {'E': 'Twisted-*', 'M': 'twisted'},
-               'Twisted Web':  {'E': 'Twisted_Web*', 'M': 'twisted/web'},
-               'Reportlab':    {'E': 'Reportlab*', 'M': 'reportlab'},
-               'AptUrl':       {'E': 'apturl-*', 'M': 'AptUrl'},
-               'CodeHack':     {'E': 'CodeHack-*', 'M': 'codehack'},
-               'Onboard':      {'E': 'onboard-*', 'M': 'Onboard'},
-               'python-apt':   {'E': 'python_apt*', 'M': 'apt'},
-               'ccsm':         {'E': 'ccsm*', 'M': 'ccm'},
-               'pysqlite2':    {'E': 'pysqlite*', 'M': 'pysqlite2'}}
+PACKAGES_DB = {
+    'AptUrl':         {'E': 'apturl-*', 'M': 'AptUrl'},
+    'ccsm':           {'E': 'ccsm*', 'M': 'ccm'},
+    'CodeHack':       {'E': 'CodeHack-*', 'M': 'codehack'},
+    'logilab-common': {'E': 'logilab_common*', 'M': 'logilab'},
+    'logilab-astng':  {'E': 'logilab_astng*', 'M': 'logilab/astng'},
+    'Onboard':        {'E': 'onboard-*', 'M': 'Onboard'},
+    'pysqlite2':      {'E': 'pysqlite*', 'M': 'pysqlite2'},
+    'python-apt':     {'E': 'python_apt*', 'M': 'apt'},
+    'Reportlab':      {'E': 'Reportlab*', 'M': 'reportlab'},
+#    'Twisted Core':   {'E': 'Twisted_Core*', 'M': 'twisted'},
+    'Twisted Core':  {'E': 'Twisted-*', 'M': 'twisted'},
+    'Twisted Web':    {'E': 'Twisted_Web*', 'M': 'twisted/web'},
+}

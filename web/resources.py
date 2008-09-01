2770f2cff2d379e24f6d2a25643e4145c6b21d7d
Hervé Cauwelier <herve@itaapy.com>
2008-09-01 19:22:10 +0200

    web: negative object comparison must be explicitly implemented

--- web/resources.py
+++ web/resources.py
@@ -164,6 +164,10 @@ class Resource(object):
         return self.get_canonical_path() == resource.get_canonical_path()
 
 
+    def __ne__(self, node):
+        return not self.__eq__(node)
+
+
     #######################################################################
     # API / Views
     #######################################################################

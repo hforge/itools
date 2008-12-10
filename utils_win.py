26b08e369a94862d01ad9f5951c117bc3292a375
J. David Ibanez <jdavid@itaapy.com>
2008-12-10 12:40:03 +0100

    Allow to install itools even if pywin is not installed.

--- utils_win.py
+++ utils_win.py
@@ -16,9 +16,12 @@
 
 # Import from the Standard Library
 from os import getpid
-from win32api import OpenProcess
-from win32con import PROCESS_QUERY_INFORMATION, PROCESS_VM_READ
-from win32process import GetProcessTimes, GetProcessMemoryInfo
+try:
+    from win32api import OpenProcess
+    from win32con import PROCESS_QUERY_INFORMATION, PROCESS_VM_READ
+    from win32process import GetProcessTimes, GetProcessMemoryInfo
+except ImportError:
+    print 'Warning: win32 is not installed'
 
 
 

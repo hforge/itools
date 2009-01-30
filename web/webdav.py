e535568292f5c3e74ce8dfd35187662dd659272b
Hervé Cauwelier <herve@itaapy.com>
2009-01-30 21:24:47 +0100

    webdav: move LOCK and UNLOCK to the new request handler

--- web/webdav.py
+++ web/webdav.py
@@ -15,15 +15,13 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Import from itools
-from itools.http import Conflict
+from itools.http import ClientError, BadRequest, Conflict
 from server import RequestMethod, register_method, find_view_by_method
 
 
 # This module is meant to provide support for the WebDav protocol.  Though so
 # far it only does the minimum required for the "external editor" of ikaaro to
-# work; it even includes an implementation of the PUT method, which probably
-# does not belong here.  Also, there is much code duplicated through the
-# LOCK, UNLOCK and PUT methods which may be refactored.
+# work.
 
 
 
@@ -46,113 +44,54 @@ lock_body = """<?xml version="1.0" encoding="utf-8" ?>
 
 
 
+class Locked(ClientError):
+    code = 423
+    title = 'Locked'
+
+
+
 class LOCK(RequestMethod):
 
+    method_name = 'LOCK'
+
+
     @classmethod
-    def handle_request(cls, server, context):
-        response = context.response
-
-        # (1) The requested resource
-        cls.find_resource(server, context)
+    def check_conditions(cls, server, context):
         resource = context.resource
-
-        # (2) Access Control
-        ac = resource.get_access_control()
-        if not ac.is_allowed_to_lock(context.user, resource):
-            # XXX Should it be Unauthorized (401) or Forbidden (403) ?
-            response.set_status(401)
-            response.set_header('content-length', 0)
-            response.set_body(None)
-            return response
-
-        # (3) Check whether the resource is already locked
         if resource.is_locked():
-            response.set_status(423)
-            response.set_header('content-length', 0)
-            response.set_body(None)
-            return response
+            raise Locked
 
-        # (4) Lock the resource
-        try:
-            lock = resource.lock()
-        except:
-            server.log_error(context)
-            server.abort_transaction(context)
-            response.set_status(500)
-            response.set_header('content-length', 0)
-            response.set_body(None)
-            return response
 
-        # (5) Commit transaction
-        cls.commit_transaction(server, context)
-
-        # (6) Ok
-        response.set_status(200)
-        response.set_header('Content-Type', 'text/xml; charset="utf-8"')
-        response.set_header('Lock-Token', 'opaquelocktoken:%s' % lock)
-        entity = lock_body % {'owner': context.user.name, 'locktoken': lock}
-        response.set_header('content-length', len(entity))
-        response.set_body(entity)
-        return response
+    @classmethod
+    def check_transaction(cls, server, context):
+        return getattr(context, 'commit', True) and context.status < 400
 
 
 
 class UNLOCK(RequestMethod):
 
+    method_name = 'UNLOCK'
+
     @classmethod
-    def handle_request(cls, server, context):
-        response = context.response
-
-        # (1) The requested resource
-        cls.find_resource(server, context)
+    def check_conditions(cls, server, context):
         resource = context.resource
-
-        # (2) Access Control
-        ac = resource.get_access_control()
-        if not ac.is_allowed_to_lock(context.user, resource):
-            # XXX Should it be Unauthorized (401) or Forbidden (403) ?
-            response.set_status(401)
-            response.set_header('content-length', 0)
-            response.set_body(None)
-            return response
-
-        # (3) Check whether the resource is already locked
         if not resource.is_locked():
-            # FIXME This probably not the good response
-            response.set_status(423)
-            response.set_header('content-length', 0)
-            response.set_body(None)
-            return response
-
-        # (4) Check wether we have the right key
-        key = context.request.get_header('Lock-Token')
+            raise Conflict
+        # Check wether we have the right key
+        request = context.request
+        key = request.get_header('Lock-Token')
+        if key is None:
+            raise BadRequest
         key = key[len('opaquelocktoken:'):]
         lock = resource.get_lock()
         if lock.key != key:
-            # FIXME Send some nice response to the client
-            raise ValueError, 'can not unlock resource, wrong key'
+            # FIXME find the good response
+            raise BadRequest
 
-        # (5) Unlock
-        try:
-            resource.unlock()
-        except:
-            server.log_error(context)
-            server.abort_transaction(context)
-            response.set_status(500)
-            response.set_header('content-length', 0)
-            response.set_body(None)
-            return response
 
-        # (6) Commit transaction
-        cls.commit_transaction(server, context)
-
-        # (7) Ok
-        response.set_status(204)
-        response.set_header('Content-Type', 'text/xml; charset="utf-8"')
-        response.set_header('Lock-Token', 'opaquelocktoken:%s' % lock)
-        response.set_header('content-length', 0)
-        response.set_body(None)
-        return response
+    @classmethod
+    def check_transaction(cls, server, context):
+        return getattr(context, 'commit', True) and context.status < 400
 
 
 

f1a9af01e9aad00a118b5696aa4d12ba5d56f94b
J. David Ibanez <jdavid@itaapy.com>
2009-06-19 12:08:57 +0200

    ical: remove code (moved to ikaaro)

--- ical/__init__.py
+++ ical/__init__.py
@@ -22,14 +22,11 @@ resources in standard type iCalendar specified in the RFC 2445
 """
 
 # Import from itools
-from gridlayout import get_grid_data
 from icalendar import iCalendar
 from table import Record, icalendarTable
 from types import DateTime, Time
 
 __all__ = [
-    # Functions
-    'get_grid_data',
     # DataTypes
     'DateTime',
     'Time',

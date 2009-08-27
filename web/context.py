43e05ffc13614d78565b01cbe145abb4f4bf40ab
J. David Ibanez <jdavid@itaapy.com>
2009-08-27 12:27:12 +0200

    web: get the root resource properly, drop get_link
    Use "context.get_resource('/')" instead of "context.host"
    
    Drop "WebContext.get_link(resource)", instead we will simply use
    "resource.path"

--- web/context.py
+++ web/context.py
@@ -141,15 +141,6 @@ class WebContext(HTTPContext):
                 scripts.append(script)
 
 
-    def get_link(self, resource):
-        """Return a link to the given resource, from the given context.
-        """
-        # FIXME This method should give an error if the given resource is
-        # not within the site root.
-        host = self.host
-        return '/%s' % host.get_pathto(resource)
-
-
     #######################################################################
     # Lazy load
     #######################################################################
@@ -256,8 +247,8 @@ class WebContext(HTTPContext):
             if type(body) is str:
                 body = XMLParser(body, doctype=xhtml_doctype)
 
-            skin = self.host.skin
-            body = skin.render(body, self)
+            root = self.get_resource('/')
+            body = root.skin.render(body, self)
         else:
             is_xml = isinstance(body, (list, GeneratorType, XMLParser))
             if is_xml:

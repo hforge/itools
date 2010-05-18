6b88fbbebeb086a7657cd1d126deb580adb81eb2
David Versmisse <david.versmisse@itaapy.com>
2010-05-18 12:15:41 +0200

    web: suppress LOCK and UNLOCK

--- web/__init__.py
+++ web/__init__.py
@@ -24,8 +24,6 @@ from messages import INFO, ERROR, MSG_MISSING_OR_INVALID
 from resources import Resource, RootResource
 from server import WebServer
 from views import BaseView, BaseForm, STLView, STLForm
-from webdav import lock_body
-
 
 __all__ = [
     'AccessControl',
@@ -49,7 +47,5 @@ __all__ = [
     # Messages
     'INFO',
     'ERROR',
-    'MSG_MISSING_OR_INVALID',
-    # Webdav
-    'lock_body',
+    'MSG_MISSING_OR_INVALID'
     ]

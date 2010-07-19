17096739da4d7aa6e63d22ebf27632c3d5f5cf94
David Versmisse <david.versmisse@itaapy.com>
2010-07-19 11:28:29 +0200

    abnf/gss: rename GSS -> Node, add data

--- abnf/gss.c
+++ abnf/gss.c
@@ -4,26 +4,30 @@
 
 typedef struct
 {
-  PyObject_HEAD int number;
-} GSS;
+  PyObject_HEAD
+  PyObject * data;
+} Node;
 
 
 static void
-GSS_dealloc (GSS * self)
+Node_dealloc (Node * self)
 {
+  Py_XDECREF (self->data);
   self->ob_type->tp_free ((PyObject *) self);
 }
 
 
 static PyObject *
-GSS_new (PyTypeObject * type, PyObject * args, PyObject * kwds)
+Node_new (PyTypeObject * type, PyObject * args, PyObject * kwds)
 {
-  GSS *self;
+  Node *self;
 
-  self = (GSS *) type->tp_alloc (type, 0);
+  self = (Node *) type->tp_alloc (type, 0);
   if (self != NULL)
     {
-      self->number = 0;
+      /* We store None, by default */
+      Py_INCREF (Py_None);
+      self->data = Py_None;
     }
 
   return (PyObject *) self;
@@ -31,38 +35,43 @@ GSS_new (PyTypeObject * type, PyObject * args, PyObject * kwds)
 
 
 static int
-GSS_init (GSS * self, PyObject * args, PyObject * kwds)
+Node_init (Node * self, PyObject * args, PyObject * kwds)
 {
-  PyObject *first = NULL, *last = NULL, *tmp;
+  PyObject *data = NULL, *tmp;
+  static char *kwlist[] = { "data", NULL };
 
-  static char *kwlist[] = { "number", NULL };
-
-  if (!PyArg_ParseTupleAndKeywords (args, kwds, "|i", kwlist, &self->number))
+  if (!PyArg_ParseTupleAndKeywords (args, kwds, "|O", kwlist, &data))
     return -1;
+
+  if (data)
+    {
+      tmp = self->data;
+      Py_INCREF (data);
+      self->data = data;
+      Py_XDECREF (tmp);
+    }
 
   return 0;
 }
 
 
-static PyObject *
-GSS_get_number (GSS * self)
-{
-  return PyInt_FromLong (self->number);
-}
-
-
-static PyMethodDef GSS_methods[] = {
-  {"get_number", (PyCFunction) GSS_get_number, METH_NOARGS,
-   "Return the number"},
+static PyMethodDef Node_methods[] = {
   {NULL}                        /* Sentinel */
 };
 
-static PyTypeObject GSSType = {
+
+static PyMemberDef Node_members[] = {
+  {"data", T_OBJECT_EX, offsetof (Node, data), 0, "data"},
+  {NULL}                        /* Sentinel */
+};
+
+
+static PyTypeObject NodeType = {
   PyObject_HEAD_INIT (NULL) 0,  /*ob_size */
-  "gss.GSS",                    /*tp_name */
-  sizeof (GSS),                 /*tp_basicsize */
+  "gss.Node",                   /*tp_name */
+  sizeof (Node),                /*tp_basicsize */
   0,                            /*tp_itemsize */
-  (destructor) GSS_dealloc,     /*tp_dealloc */
+  (destructor) Node_dealloc,    /*tp_dealloc */
   0,                            /*tp_print */
   0,                            /*tp_getattr */
   0,                            /*tp_setattr */
@@ -78,24 +87,24 @@ static PyTypeObject GSSType = {
   0,                            /*tp_setattro */
   0,                            /*tp_as_buffer */
   Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,     /*tp_flags */
-  "GSS object",                 /* tp_doc */
+  "Node object",                /* tp_doc */
   0,                            /* tp_traverse */
   0,                            /* tp_clear */
   0,                            /* tp_richcompare */
   0,                            /* tp_weaklistoffset */
   0,                            /* tp_iter */
   0,                            /* tp_iternext */
-  GSS_methods,                  /* tp_methods */
-  0,                            /* tp_members */
+  Node_methods,                 /* tp_methods */
+  Node_members,                 /* tp_members */
   0,                            /* tp_getset */
   0,                            /* tp_base */
   0,                            /* tp_dict */
   0,                            /* tp_descr_get */
   0,                            /* tp_descr_set */
   0,                            /* tp_dictoffset */
-  (initproc) GSS_init,          /* tp_init */
+  (initproc) Node_init,         /* tp_init */
   0,                            /* tp_alloc */
-  GSS_new,                      /* tp_new */
+  Node_new,                     /* tp_new */
 };
 
 static PyMethodDef module_methods[] = {
@@ -110,7 +119,7 @@ initgss (void)
 {
   PyObject *m;
 
-  if (PyType_Ready (&GSSType) < 0)
+  if (PyType_Ready (&NodeType) < 0)
     return;
 
   m = Py_InitModule3 ("gss", module_methods,
@@ -119,6 +128,6 @@ initgss (void)
   if (m == NULL)
     return;
 
-  Py_INCREF (&GSSType);
-  PyModule_AddObject (m, "GSS", (PyObject *) & GSSType);
+  Py_INCREF (&NodeType);
+  PyModule_AddObject (m, "Node", (PyObject *) & NodeType);
 }

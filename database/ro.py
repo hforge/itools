a1800ad08cb53cf5e7dcc20bcea4328beafcd8a0
David Versmisse <david.versmisse@itaapy.com>
2010-08-03 17:38:57 +0200

    database: ROGitDatabase doesn't inherit anymore from RODatabase

--- database/ro.py
+++ database/ro.py
@@ -19,6 +19,7 @@
 
 # Import from the Standard Library
 from datetime import datetime
+from sys import getrefcount
 
 # Import from xapian
 from xapian import DatabaseOpeningError
@@ -26,14 +27,18 @@ from xapian import DatabaseOpeningError
 # Import from itools
 from itools.core import LRUCache, freeze, lazy, send_subprocess
 from itools.fs import lfs
-from itools.handlers import RODatabase
+from itools.handlers import Folder, get_handler_class_by_mimetype
 from itools.uri import Path
 from catalog import Catalog
 from registry import get_register_fields
 
 
 
-class ROGitDatabase(RODatabase):
+class ROGitDatabase(object):
+
+    # Flag to know whether to commit or not.  This is to avoid superfluos
+    # actions by the 'save' and 'abort' methods.
+    has_changed = False
 
     def __init__(self, path, size_min=4800, size_max=5200):
         # 1. Keep the path
@@ -51,13 +56,117 @@ class ROGitDatabase(RODatabase):
             raise ValueError, error
 
         # 3. Initialize the database, but chrooted
-        folder = lfs.open(self.path_data)
-        super(ROGitDatabase, self).__init__(size_min, size_max, fs=folder)
+        self.fs = lfs.open(self.path_data)
 
-        # 4. The git cache
+        # 4. A mapping from key to handler
+        self.cache = LRUCache(size_min, size_max, automatic=False)
+
+        # 5. The git cache
         self.git_cache = LRUCache(900, 1100)
 
 
+    #######################################################################
+    # Private API
+    #######################################################################
+    def _sync_filesystem(self, key):
+        """This method checks the state of the key in the cache against the
+        filesystem. Synchronizes the state if needed by discarding the
+        handler, or raises an error if there is a conflict.
+
+        Returns the handler for the given key if it is still in the cache
+        after all the tests, or None otherwise.
+        """
+        # If the key is not in the cache nothing can be wrong
+        handler = self.cache.get(key)
+        if handler is None:
+            return None
+
+        # (1) Not yet loaded
+        if handler.timestamp is None and handler.dirty is None:
+            # Removed from the filesystem
+            if not self.fs.exists(key):
+                self._discard_handler(key)
+                return None
+            # Everything looks fine
+            # FIXME There will be a bug if the file in the filesystem has
+            # changed to a different type, so the handler class may not match.
+            return handler
+
+        # (2) New handler
+        if handler.timestamp is None and handler.dirty is not None:
+            # Everything looks fine
+            if not self.fs.exists(key):
+                return handler
+            # Conflict
+            error = 'new file in the filesystem and new handler in the cache'
+            raise RuntimeError, error
+
+        # (3) Loaded but not changed
+        if handler.timestamp is not None and handler.dirty is None:
+            # Removed from the filesystem
+            if not self.fs.exists(key):
+                self._discard_handler(key)
+                return None
+            # Modified in the filesystem
+            mtime = self.fs.get_mtime(key)
+            if mtime > handler.timestamp:
+                self._discard_handler(key)
+                return None
+            # Everything looks fine
+            return handler
+
+        # (4) Loaded and changed
+        if handler.timestamp is not None and handler.dirty is not None:
+            # Removed from the filesystem
+            if not self.fs.exists(key):
+                error = 'a modified handler was removed from the filesystem'
+                raise RuntimeError, error
+            # Modified in the filesystem
+            mtime = self.fs.get_mtime(key)
+            if mtime > handler.timestamp:
+                error = 'modified in the cache and in the filesystem'
+                raise RuntimeError, error
+            # Everything looks fine
+            return handler
+
+
+    def _discard_handler(self, key):
+        """Unconditionally remove the handler identified by the given key from
+        the cache, and invalidate it (and free memory at the same time).
+        """
+        handler = self.cache.pop(key)
+        # Invalidate the handler
+        handler.__dict__.clear()
+
+
+    def _rollback(self):
+        """To be called when something goes wrong while saving the changes.
+        """
+        raise NotImplementedError
+
+
+    def _abort_changes(self):
+        """To be called to abandon the transaction.
+        """
+        raise NotImplementedError
+
+
+    def _cleanup(self):
+        """For maintenance operations, this method is automatically called
+        after a transaction is committed or aborted.
+        """
+#       import gc
+#       from itools.core import vmsize
+#       print 'RODatabase._cleanup (0): % 4d %s' % (len(self.cache), vmsize())
+#       print gc.get_count()
+        self.make_room()
+#       print 'RODatabase._cleanup (1): % 4d %s' % (len(self.cache), vmsize())
+#       print gc.get_count()
+
+
+    #######################################################################
+    # Public API
+    #######################################################################
     def normalize_key(self, path, __root=Path('/')):
         # Performance is critical so assume the path is already relative to
         # the repository.
@@ -67,6 +176,166 @@ class ROGitDatabase(RODatabase):
             raise ValueError, err % path
 
         return '/'.join(key)
+
+
+    def push_handler(self, key, handler):
+        """Adds the given resource to the cache.
+        """
+        handler.database = self
+        handler.key = key
+        # Folders are not stored in the cache
+        if isinstance(handler, Folder):
+            return
+        # Store in the cache
+        self.cache[key] = handler
+
+
+    def make_room(self):
+        """Remove handlers from the cache until it fits the defined size.
+
+        Use with caution. If the handlers we are about to discard are still
+        used outside the database, and one of them (or more) are modified, then
+        there will be an error.
+        """
+        # Find out how many handlers should be removed
+        size = len(self.cache)
+        if size < self.cache.size_max:
+            return
+
+        # Discard as many handlers as needed
+        n = size - self.cache.size_min
+        for key, handler in self.cache.iteritems():
+            # Skip externally referenced handlers (refcount should be 3:
+            # one for the cache, one for the local variable and one for
+            # the argument passed to getrefcount).
+            refcount = getrefcount(handler)
+            if refcount > 3:
+                continue
+            # Skip modified (not new) handlers
+            if handler.dirty is not None:
+                continue
+            # Discard this handler
+            self._discard_handler(key)
+            # Check whether we are done
+            n -= 1
+            if n == 0:
+                return
+
+
+    def has_handler(self, key):
+        key = self.normalize_key(key)
+
+        # Synchronize
+        handler = self._sync_filesystem(key)
+        if handler is not None:
+            return True
+
+        # Ask vfs
+        return self.fs.exists(key)
+
+
+    def get_handler_names(self, key):
+        key = self.normalize_key(key)
+
+        if self.fs.exists(key):
+            names = self.fs.get_names(key)
+            return list(names)
+
+        return []
+
+
+    def get_handler_class(self, key):
+        fs = self.fs
+        mimetype = fs.get_mimetype(key)
+
+        try:
+            return get_handler_class_by_mimetype(mimetype)
+        except ValueError:
+            if fs.is_file(key):
+                from file import File
+                return File
+            elif fs.is_folder(key):
+                from folder import Folder
+                return Folder
+
+        raise ValueError
+
+
+    def get_handler(self, key, cls=None):
+        key = self.normalize_key(key)
+
+        # Synchronize
+        handler = self._sync_filesystem(key)
+        if handler is not None:
+            # Check the class matches
+            if cls is not None and not isinstance(handler, cls):
+                error = "expected '%s' class, '%s' found"
+                raise LookupError, error % (cls, handler.__class__)
+            # Cache hit
+            self.cache.touch(key)
+            return handler
+
+        # Check the resource exists
+        if not self.fs.exists(key):
+            raise LookupError, 'the resource "%s" does not exist' % key
+
+        # Folders are not cached
+        if self.fs.is_folder(key):
+            if cls is None:
+                cls = Folder
+            folder = cls(key, database=self)
+            return folder
+
+        # Cache miss
+        if cls is None:
+            cls = self.get_handler_class(key)
+        # Build the handler and update the cache
+        handler = object.__new__(cls)
+        self.push_handler(key, handler)
+
+        return handler
+
+
+    def get_handlers(self, key):
+        base = self.normalize_key(key)
+        for name in self.get_handler_names(base):
+            key = self.fs.resolve2(base, name)
+            yield self.get_handler(key)
+
+
+    def touch_handler(self, key, handler=None):
+        """Report a modification of the key/handler to the database.  We must
+        pass the handler because of phantoms.
+        """
+        raise NotImplementedError, 'cannot set handler'
+
+
+    def set_handler(self, key, handler):
+        raise NotImplementedError, 'cannot set handler'
+
+
+    def del_handler(self, key):
+        raise NotImplementedError, 'cannot del handler'
+
+
+    def copy_handler(self, source, target):
+        raise NotImplementedError, 'cannot copy handler'
+
+
+    def move_handler(self, source, target):
+        raise NotImplementedError, 'cannot move handler'
+
+
+    def save_changes(self):
+        raise NotImplementedError
+
+
+    def abort_changes(self):
+        if not self.has_changed:
+            return
+
+        self._abort_changes()
+        self._cleanup()
 
 
     def push_phantom(self, key, handler):

4d37ec8c518e00e40cac0bd99b2a7bcfa5ba8db2
Norman Khine <khinester@aqoon.local>
2010-11-14 00:13:15 +0100

    handlers#1058: checkid supports Turkish chars
    Added support for these characters ő ļ ş ă ğ Ş ı

--- handlers/utils.py
+++ handlers/utils.py
@@ -16,10 +16,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-src = (ur"""ÄÅÁÀÂÃĀäåáàâãāÇçÉÈÊËĒéèêëēÍÌÎÏĪíìîïīÑñÖÓÒÔÕØŌöóòôõøōÜÚÙÛŪüúùûū"""
-       ur"""ÝŸȲýÿȳŽž""")
-dst = (ur"""AAAAAAAaaaaaaaCcEEEEEeeeeeIIIIIiiiiiNnOOOOOOOoooooooUUUUUuuuuu"""
-       ur"""YYYyyyZz""")
+src = (ur"""ÄÅÁÀÂÃĀäåáàâãāăÇçÉÈÊËĒéèêëēğÍÌÎÏĪíìîïīıļÑñÖÓÒÔÕØŌöóòôõøōőÜÚÙÛŪüúùûū"""
+       ur"""ŞşţÝŸȲýÿȳŽž""")
+dst = (ur"""AAAAAAAaaaaaaaaCcEEEEEeeeeegIIIIIiiiiiilNnOOOOOOOooooooooUUUUUuuuuu"""
+       ur"""SstYYYyyyZz""")
 
 transmap = {}
 for i in range(len(src)):

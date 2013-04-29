337fde4c6fb48dbd49300b4252d4386f892bbca3
J. David Ibáñez <jdavid.ibp@gmail.com>
2013-04-29 22:50:14 +0200

    web: make 'log_access' robust to when host is None
    Sometimes we get these error:
    
    (process:28298): libsoup-WARNING **: soup-socket.c:1361: could not get remote address: Bad file descriptor
    (process:28298): libsoup-CRITICAL **: soup_address_get_physical: assertion `SOUP_IS_ADDRESS (addr)' failed
    
    When this happens the 'host' parameter passed to the
    'WebServer.log_access' method will be None.

--- web/server.py
+++ web/server.py
@@ -56,7 +56,8 @@ class WebServer(SoupServer):
 
 
     def log_access(self, host, request_line, status_code, body_length):
-        host = host.split(',', 1)[0].strip()
+        if host:
+            host = host.split(',', 1)[0].strip()
         now = strftime('%d/%b/%Y:%H:%M:%S')
         message = '%s - - [%s] "%s" %d %d\n' % (host, now, request_line,
                                                 status_code, body_length)

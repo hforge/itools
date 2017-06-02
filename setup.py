d7fd9a4edd03d7a53deb4625c3774252ce42732a
Taverne Sylvain <sylvain@agicia.com>
2017-06-02 16:21:40 +0200

    Release 0.77.4

--- setup.py
+++ setup.py
@@ -245,7 +245,7 @@ if __name__ == '__main__':
         else:
             package_data['itools'].append(join_path(*path[1:]))
     setup(name="itools",
-          version="0.77.3",
+          version="0.77.4",
           # Metadata
           author="J. David Ibáñez",
           author_email="jdavid.ibp@gmail.com" ,

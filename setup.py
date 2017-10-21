88085df061bafa6bfd56cfa3e3819c6605e87417
J. David Ibáñez <jdavid.ibp@gmail.com>
2017-10-21 19:33:59 +0200

    Release 0.77.7

--- setup.py
+++ setup.py
@@ -245,7 +245,7 @@ if __name__ == '__main__':
         else:
             package_data['itools'].append(join_path(*path[1:]))
     setup(name="itools",
-          version="0.77.6",
+          version="0.77.7",
           # Metadata
           author="J. David Ibáñez",
           author_email="jdavid.ibp@gmail.com" ,

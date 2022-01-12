b5e3105207195315ef141b182b13e6f3f05c62f0
mathieu.pequin <mathieu.pequin@bepatient.com>
2022-01-12 19:11:25 +0100

    (#MOBI-6291)  fix attempt

--- setup.py
+++ setup.py
@@ -226,7 +226,7 @@ if __name__ == '__main__':
         else:
             package_data['itools'].append(join_path(*path[1:]))
     setup(name="itools",
-          version="0.79.3",
+          version="0.78.0",
           # Metadata
           author="J. David Ibáñez",
           author_email="jdavid.ibp@gmail.com" ,

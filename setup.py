453503200de9c47bc71bf2a2238882b7401527d4
mathieu.pequin <mathieu.pequin@bepatient.com>
2022-06-02 10:55:56 +0200

    Upgrade pillow

--- setup.py
+++ setup.py
@@ -236,7 +236,7 @@ if __name__ == '__main__':
         else:
             package_data['itools'].append(join_path(*path[1:]))
     setup(name="itools",
-          version="0.80.3",
+          version="0.80.4",
           # Metadata
           author="J. David Ibáñez",
           author_email="jdavid.ibp@gmail.com" ,
